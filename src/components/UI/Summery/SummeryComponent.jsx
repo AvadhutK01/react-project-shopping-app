14dff73427568af338ab06fa5d3e1b08d000a0f5
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-11 16:43:41 +0530

    Summery component implementation

--- src/components/UI/Summery/SummeryComponent.jsx
+++ src/components/UI/Summery/SummeryComponent.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const SummeryComponent = () => {
     return (
-        <div>SummeryComponent</div>
+        <div className='shadow-xl'>
+            <div className=' flex flex-col items-center justify-center'>
+                <img src="cover2.jpg" className='h-[30rem] w-full' alt="img" />
+                <div className='w-[30rem] text-black border-2 p-7 rounded-xl bg-white absolute top-72 shadow-2xl'>
+                    <p className='font-bold'>"Elevate Your Style, Anytime, Anywhere: Unleash Your Wardrobe Wonders with cloths&fashion"</p>
+                    <p>
+                        Upgrade your style effortlessly with our curated fashion delivered to you!\
+                        Explore a variety of trendy and timeless clothing,
+                        ensuring a seamless and delightful shopping experience from
+                        the comfort of your home.</p>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
--- src/components/main/MainComponent.jsx
+++ src/components/main/MainComponent.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeaderComponent from '../UI/Header/HeaderComponent'
+import SummeryComponent from '../UI/Summery/SummeryComponent'
 
 const MainComponent = () => {
     return (
         <div>
             <HeaderComponent />
+            <SummeryComponent />
         </div>
     )
 }
